25ff9e0658090b73c75ee8eebe8e17fb0f348207
fabior <fabio.rosa.mail@gmail.com>
2014-03-14 15:31:50 +0100

    -refinement & links

--- exobash_vlad.sh
+++ exobash_vlad.sh
@@ -14,7 +14,7 @@ TYPE=indels
 WORK=~/workspace/vladimir
 #RefGENOME=~/bin/hg19/ucsc.hg19.fasta
 #RefPrefix=ucsc.hg19
-RefGENOME=~/bin/hs37d5/hs37d5.fa
+RefGENOME=~/bin/hs37d5/hs37d5
 RefPrefix=hs37d5
 
 #memory alloc.
@@ -27,20 +27,57 @@ PICARD=~/bin/picard
 echo "$# parameters...";
 
 
+# general comments:
+# lh3
+# 10-17-2010, 08:31 PM
+# The following is what I would recommend for Illumina:
+# 1. Do alignment with novoalign or bwa. Mosaik is also great, but unfortunately it does not write soft clippings, which will affect programs at a later step. Bowtie is not recommended because it does not do gapped alignment.
+# 2. If you have bandwidth, do realignment with GATK. If you do not, it actually does not matter too much. The major downside of not doing realignment is you may get confusing alignment in an alignment viewer (the most frequent question is "why the indel caller is calling an indel when there is only 1 read supporting that?").
+# 3. Cap base quality BAQ (with samtools).
+# 4. Call SNPs with whatever SNP caller. It does not matter too much when indels are cleaned.
+# 5. Call indels with dindel. The Dindel group has shown convincing evidence that it is clearly better. When I evaluate it by myself, I am convinced again. It is much more sensitive than gatk realigner+IndelGenotyperV2; its specificity is also better. For exonome sequencing the difference is probably smaller because the hard regions are mostly related to repeats.
+
+
+# http://www.biostars.org/p/1268/
+# Question: What is the best pipeline for human whole exome sequencing?
+
+
 #steps
-QC=true
+QC=false
 HGindex=false
 MAP2Bam=false
 
 PICARDPreproc=false
+
+
 INDELRealign=false
 #
-# fixmate done by indelrealigner
+# FIXMATE step
+# http://gatkforums.broadinstitute.org/discussion/1562/need-to-run-a-step-with-fixmateinformation-after-realignment-step
+# Mark_DePristo Posts: 153 admin
+# September 2012 Answer ?
+# The indel realigner fixes mates itself, so the file is valid after realignment.
+# How this is accomplished is a bit of magic code from Eric Banks and myself from several years ago.
+# A previous version of the realigner did require you to run two passes, but that was before the magic code was written.
 FIXMATE=false
+
+# Michael.James.Clark
+# 10-26-2010, 11:41 AM
+# Hi Heng,
+# 3. Cap base quality BAQ (with samtools).
+# What do you mean by "cap base quality"?
+# Can you give more detailed suggestions about how to effectively use BAQ?
+# We would like to include it in our pipeline but are unsure about how best to utilize it.
+#
+# drio
+# 10-27-2010, 04:53 AM
+# Here. http://seqanswers.com/forums/showpost.php?p=27334&postcount=26
 MDTag=false
+
 BASERecal=false
 #
 SNPCall=false
+
 COMBINEALL=false
 # or
 PEDSNPCall=false
@@ -63,77 +100,122 @@ fi
 
 if $QC; then
 	#
-	~/bin/FastQC/fastqc $(MAP1)
-	~/bin/FastQC/fastqc $(MAP2)
+	~/bin/FastQC/fastqc --noextract ${MAP1}.fq ${MAP2}.fq
 	#
 	# trimmomatic
+	java -jar ~/bin/trimmomatic/trimmomatic.jar PE -phred33 ${MAP1}.fq ${MAP2}.fq ${MAP1}_paired.fq ${MAP1}_unpaired.fq ${MAP2}_paired.fq ${MAP2}_unpaired.fq LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+	#
+	~/bin/FastQC/fastqc --noextract 51960F_1_paired.fq 51960F_2_paired.fq
 fi
 
+
+further justification for BWA mem
+http://www.bioplanet.com/gcat/reports/80/alignment/250bp-pe-small-indel/bwa/compare-68-72
+
+# http://lists.idyll.org/pipermail/ngs-2013/2013-June/000099.html
+#On Tue, Jun 25, 2013 at 2:04 PM, Rayan Chikhi <rchikhi at gmail.com> wrote:
+#
+# Yes, the aln/sampe commands invoke the original BWA algorithm
+# (backtracking, from 2009), and the mem command invokes the latest BWA
+# improvement (MEM algorithm, 2013).
+# See http://bio-bwa.sourceforge.net/bwa.shtml#2
+# I think it is fair to say that mem should replace aln/sampe for everything
+# now. (except maybe on older, < 70 bp reads)
+#
+# Rayan
 if $MAP2Bam; then
 	# mapping su bundled-hg19 con bwa o bowtie
 	#1 Align samples to genome (BWA), generates SAI files: .fastq ==> .sai
-	~/bin/bwa/bwa aln -t $(NCORES) $(RefGENOME) -b $(MAP1).bam > $(MAP1).sai
-	~/bin/bwa/bwa aln -t $(NCORES) $(RefGENOME) -b $(MAP2).bam > $(MAP2).sai
-	
+	#1.1 if reads in bam format...
+	if false; then
+	~/bin/bwa/bwa aln -t ${NCORES} ${RefGENOME} -b ${MAP1}.bam > ${MAP1}.sai
+	~/bin/bwa/bwa aln -t ${NCORES} ${RefGENOME} -b ${MAP2}.bam > ${MAP2}.sai
 	#2 Convert SAI to SAM (BWA): r1.sai, r2.sai ==> .sam 
-	~/bin/bwa/bwa sampe -P $(RefGENOME) $(MAP1).sai $(MAP2).sai $(MAP1).bam $(MAP2).bam > $(MAPPED).sam
-	#this works with specific versions of bwa: 0.6.2/0.5.10 otherwise ==> picard/AddOrReplaceReadGroups
-	#~/bin/bwa/bwa sampe -P $(RefGENOME) -r '@RG\tID:foo\tSM:bar' $(MAP1).sai $(MAP2).sai $(MAP1).bam $(MAP2).bam > $(MAPPED).sam
+	~/bin/bwa/bwa sampe -P ${RefGENOME} ${MAP1}.sai ${MAP2}.sai ${MAP1}.bam ${MAP2}.bam > ${MAPPED}.sam
+
+	fi
+	#1.2 if reads in fastq format...
+	if true; then
+	~/bin/bwa/bwa aln -t ${NCORES} ${RefGENOME} ${MAP1}.fq > ${MAP1}.sai
+	~/bin/bwa/bwa aln -t ${NCORES} ${RefGENOME} ${MAP2}.fq > ${MAP2}.sai
+#2 Convert SAI to SAM (BWA): r1.sai, r2.sai ==> .sam 
+	~/bin/bwa/bwa sampe -P ${RefGENOME} ${MAP1}.sai ${MAP2}.sai ${MAP1}.fq ${MAP2}.fq > ${MAPPED}.sam
+ 	fi
+	
+		#this works with specific versions of bwa: 0.6.2/0.5.10 otherwise ==> picard/AddOrReplaceReadGroups
+	#~/bin/bwa/bwa sampe -P ${RefGENOME} -r '@RG\tID:foo\tSM:bar' ${MAP1}.sai ${MAP2}.sai ${MAP1}.bam ${MAP2}.bam > ${MAPPED}.sam
 	
 	#3 Convert SAM to BAM binary format (SAM Tools): .sam ==> .bam
-	~/bin/samtools/samtools import $(RefGENOME).fai $(MAPPED).sam $(MAPPED).bam 
+	~/bin/samtools/samtools import ${RefGENOME}.fai ${MAPPED}.sam ${MAPPED}.bam 
 
 	#4 Sort BAM (SAM Tools, second param is a prefix): .bam ==> .sort.bam 
-	~/bin/samtools/samtools sort $(MAPPED).bam $(MAPPED).sort
+	~/bin/samtools/samtools sort ${MAPPED}.bam ${MAPPED}.sort
 
 	#5 Index BAM (SAM Tools): .bam ==> .bai
-	~/bin/samtools/samtools index $(MAPPED).sort.bam
+	~/bin/samtools/samtools index ${MAPPED}.sort.bam
 fi
 
 #
 #   lexy.bash
 #
 
+# http://www.broadinstitute.org/gatk/guide/best-practices#tutorials_mapdedup2909
+#Fix a BAM that is not indexed or not sorted, has not had duplicates marked, or is lacking read group information.
+#These steps can be performed independently of each other but this order is recommended.
+#Prerequisites:  Installed Picard tools
+#
+#Steps:
+#    Sort the aligned reads by coordinate order
+#    Mark duplicates
+#    Add read group information
+#    Index the BAM file
+
 if $PICARDPreproc; then
 	#5.1 add grouping info in header (picard): .sort.bam ==> .group.bam, (.group.bai)
-	java $MEM -Djava.io.tmpdir=/tmp -jar ${PICARD}/AddOrReplaceReadGroups.jar I=$(MAPPED).lexy.bam O=$(MAPPED).group.bam LB=whatever PL=illumina PU=r12 SM=$FASTQ1NAME CREATE_INDEX=true SO=coordinate VALIDATION_STRINGENCY=LENIENT
+	java ${MEM} -Djava.io.tmpdir=/tmp -jar ${PICARD}/AddOrReplaceReadGroups.jar I=${MAPPED}.bam O=${MAPPED}.group.bam LB=whatever PL=illumina PU=r12 SM=$FASTQ1NAME CREATE_INDEX=true SO=coordinate VALIDATION_STRINGENCY=LENIENT
 
 	#5.2 su cui marco i PCR duplicati group.bam ==> .marked.bam, .marked.bai
-	java $MEM -Djava.io.tmpdir=/tmp -jar ${PICARD}/MarkDuplicates.jar INPUT=$(MAPPED).group.bam OUTPUT=$(MAPPED).marked.bam METRICS_FILE=$(MAPPED).metrics CREATE_INDEX=true ASSUME_SORTED=true VALIDATION_STRINGENCY=LENIENT
+	java ${MEM} -Djava.io.tmpdir=/tmp -jar ${PICARD}/MarkDuplicates.jar INPUT=${MAPPED}.group.bam OUTPUT=${MAPPED}.marked.bam METRICS_FILE=${MAPPED}.metrics CREATE_INDEX=true ASSUME_SORTED=true VALIDATION_STRINGENCY=LENIENT
 	#??? create index x gatk ????
 	#
-	#java $MEM -Djava.io.tmpdir=/tmp -jar ${PICARD}/ReorderSam.jar I=$(MAPPED).marked.bam O=$(MAPPED).lexy_marked.bam REFERENCE= ${RefGENOME} CREATE_INDEX=true
+	#java ${MEM} -Djava.io.tmpdir=/tmp -jar ${PICARD}/ReorderSam.jar I=${MAPPED}.marked.bam O=${MAPPED}.lexy_marked.bam REFERENCE= ${RefGENOME} CREATE_INDEX=true
 fi
 
 # here GATK needs .bai index
 if $INDELRealign; then
 	#6 Identify target regions for realignment (Genome Analysis Toolkit): .marked.bam ==> .intervals
-	#  performance 500MB bam/~7000000 reads: 1 core -> 3hh 
-	java $MEM -jar ${GATK} -T RealignerTargetCreator -nt $NCORES -R ${RefGENOME} -I $(MAPPED).marked.bam -o $(MAPPED).intervals
+	#  performance 500MB bam/~7000000 reads: 1 core a-> 3hh 
+	java ${MEM} -jar ${GATK} -T RealignerTargetCreator -nt $NCORES \
+		-R ${RefGENOME}.fa \
+		-I ${MAPPED}.marked.bam \
+		-o ${MAPPED}.intervals
+		#--knonwn
+		#-L
 	
 	#7 Realign BAM to get better Indel calling (Genome Analysis Toolkit): .marked.bam, .intervals ==> .realn.bam
-	java -jar ${GATK} -T IndelRealigner -R ${RefGENOME} -I $(MAPPED).marked.bam -targetIntervals $(MAPPED).intervals -o $(MAPPED).realn.bam
+	java -jar ${GATK} -T IndelRealigner -R ${RefGENOME}.fa -I ${MAPPED}.marked.bam -targetIntervals ${MAPPED}.intervals -o ${MAPPED}.realn.bam
 fi
 
 if $FIXMATE; then
 	#7.1 using paired end data, the mate information must be fixed: .realn.bam ==> .mated.bam
-	java $MEM -Djava.io.tmpdir=/tmp -jar ${PICARD}/FixMateInformation.jar INPUT=$(MAPPED).realn.bam OUTPUT=$(MAPPED).mated.bam SO=coordinate CREATE_INDEX=true VALIDATION_STRINGENCY=LENIENT
+	java ${MEM} -Djava.io.tmpdir=/tmp -jar ${PICARD}/FixMateInformation.jar INPUT=${MAPPED}.realn.bam OUTPUT=${MAPPED}.mated.bam SO=coordinate CREATE_INDEX=true VALIDATION_STRINGENCY=LENIENT
 fi
+
 
 if $MDTag; then
 	#7.5 substantially improve SNP specificity with MD/NM editing: .mated.bam ==> .baq.bam
-	~/bin/samtools/samtools calmd -Abr $(MAPPED).mated.bam ${RefGENOME} > $(MAPPED).baq.bam
+	~/bin/samtools/samtools calmd -Abr ${MAPPED}.mated.bam ${RefGENOME} > ${MAPPED}.baq.bam
 	#8 Reindex the realigned BAM (SAM Tools)
-	~/bin/samtools/samtools index $(MAPPED).baq.bam
+	~/bin/samtools/samtools index ${MAPPED}.baq.bam
 fi
 
 if $BASERecal; then
 	#8.1 quality score recalibration: count covariates 
-	java $MEM -jar ${GATK} \
+	java ${MEM} -jar ${GATK} \
 	-l INFO \
 	-R ${RefGENOME} \
 	--DBSNP dbsnp_137.hg19.vcf \
-	-I $(MAPPED).baq.bam \
+	-I ${MAPPED}.baq.bam \
 	-T CountCovariates \
 	-cov ReadGroupCovariate \
 	-cov QualityScoreCovariate \
@@ -142,36 +224,40 @@ if $BASERecal; then
 	-recalFile input.recal_data.csv
 
 	#8.1 quality score recalibration: table recalibration
-	java $MEM -jar ${GATK} \
+	java ${MEM} -jar ${GATK} \
 	-l INFO \
 	-R ${RefGENOME} \
-	-I $(MAPPED).baq.bam \
+	-I ${MAPPED}.baq.bam \
 	-T TableRecalibration \
-	--out -I $(MAPPED).recal.bam \
+	--out -I ${MAPPED}.recal.bam \
 	-recalFile input.recal_data.csv
 fi
 
+
+# lh3
+# 08-27-2010, 01:19 PM
+# Remember to set a quality threshold (about 50) on indels. Also the best indel caller so far is believed to be Dindel.
 if $SNPCall; then
 	#9 snp calls: .baq.bam ==> .vcf
-	java $MEM -jar ${GATK} -glm BOTH -nt $NCORES -R ${RefGENOME} -T UnifiedGenotyper \
-	-I $(MAPPED).baq.bam \
-	-D ~/bin/hg19/dbsnp_137.hg19.vcf -metrics $(MAPPED).snps.metrics -A DepthOfCoverage -A AlleleBalance \
-	-o $(MAPPED).snps.vcf  -stand_call_conf 50.0 -stand_emit_conf 10.0 -dcov 1000 \
+	java ${MEM} -jar ${GATK} -glm BOTH -nt $NCORES -R ${RefGENOME} -T UnifiedGenotyper \
+	-I ${MAPPED}.baq.bam \
+	-D ~/bin/hg19/dbsnp_137.hg19.vcf -metrics ${MAPPED}.snps.metrics -A DepthOfCoverage -A AlleleBalance \
+	-o ${MAPPED}.snps.vcf  -stand_call_conf 50.0 -stand_emit_conf 10.0 -dcov 1000 \
 	-L ~/bin/hg19/target.intervals.bed
 fi
 
 if $PEDSNPCall; then
 	#9 snp calls by family: .baq.bam ==> .vcf
-	java $MEM -jar ${GATK} -glm BOTH -nt $NCORES $PED -R ${RefGENOME} -T UnifiedGenotyper \
-	-I $(MAPPED).baq.bam \
-	-D ~/bin/hg19/dbsnp_137.hg19.vcf -metrics $(MAPPED).snps.metrics -A DepthOfCoverage -A AlleleBalance \
-	-o $(MAPPED).snps.vcf  -stand_call_conf 50.0 -stand_emit_conf 10.0 -dcov 1000 \
+	java ${MEM} -jar ${GATK} -glm BOTH -nt $NCORES $PED -R ${RefGENOME} -T UnifiedGenotyper \
+	-I ${MAPPED}.baq.bam \
+	-D ~/bin/hg19/dbsnp_137.hg19.vcf -metrics ${MAPPED}.snps.metrics -A DepthOfCoverage -A AlleleBalance \
+	-o ${MAPPED}.snps.vcf  -stand_call_conf 50.0 -stand_emit_conf 10.0 -dcov 1000 \
 	-L ~/bin/hg19/target.intervals.bed
 fi
 
 if $COMBINEALL; then
 	# merge all vcf
-	java $MEM -jar ${GATK} -R ${RefGENOME} -T CombineVariants \
+	java ${MEM} -jar ${GATK} -R ${RefGENOME} -T CombineVariants \
 	--variant 5152F/C39G1ACXX_Pavlinic_lane5152F_sequence.snps.vcf \
 	--variant 5388P/C39G1ACXX_Pavlinic_lane5388P_sequence.snps.vcf \
 	--variant 5387M/C39G1ACXX_Pavlinic_lane5387M_sequence.snps.vcf \
@@ -182,7 +268,7 @@ fi
 
 if $VSQR; then
 	#8.1 quality score recalibration: snp model
-	java $MEM -jar ${GATK} -R ${RefGENOME} -T VariantRecalibrator -nt $NCORES \
+	java ${MEM} -jar ${GATK} -R ${RefGENOME} -T VariantRecalibrator -nt $NCORES \
 		-input $PRJ.snps.vcf \
 		--maxGaussians 4 \
 		--resource:hapmap,known=false,	training=true,	truth=true,		prior=15.0	~/bin/hg19/hapmap_3.3.hg19.vcf \
@@ -194,7 +280,7 @@ if $VSQR; then
 		-tranchesFile $PRJ.snps.tranches
 	
 	# indel model
-	java $MEM -jar ${GATK} -R ${RefGENOME} -T VariantRecalibrator -nt $NCORES \
+	java ${MEM} -jar ${GATK} -R ${RefGENOME} -T VariantRecalibrator -nt $NCORES \
 		-input $PRJ.snps.vcf \
 		--maxGaussians 4 -std 10.0 -percentBad 0.12 \
 		--resource:mills,known=true,training=true,truth=true,prior=12.0 ~/bin/hg19/Mills_and_1000G_gold_standard.indels.hg19.vcf \
@@ -204,7 +290,7 @@ if $VSQR; then
 		-tranchesFile $PRJ.indels.tranches
 	
 	# apply SNP model
-	java $MEM -jar ${GATK} -R ${RefGENOME} -T ApplyRecalibration -nt $NCORES \
+	java ${MEM} -jar ${GATK} -R ${RefGENOME} -T ApplyRecalibration -nt $NCORES \
 		-input $PRJ.snps.vcf \
 		--ts_filter_level 99.0 \
 		-tranchesFile $PRJ.snps.tranches \
@@ -213,7 +299,7 @@ if $VSQR; then
 		-o $PRJ.snps.intermediate.vcf
 	
 	# apply indel model
-	java $MEM -jar ${GATK} -R ${RefGENOME} -T ApplyRecalibration -nt $NCORES \
+	java ${MEM} -jar ${GATK} -R ${RefGENOME} -T ApplyRecalibration -nt $NCORES \
 		-input $PRJ.snps.intermediate.vcf \
 		-ts_filter_level 99.9 \
 		-tranchesFile $PRJ.indels.tranches \
@@ -225,7 +311,7 @@ fi
 
 if $HARDFILTER; then
 	# Extract the SNPs from the call set
-	java $MEM -jar ${GATK} -R ${RefGENOME} -T SelectVariants -nt $NCORES \
+	java ${MEM} -jar ${GATK} -R ${RefGENOME} -T SelectVariants -nt $NCORES \
 		-V $PRJ.snps.vcf \
 	    -selectType SNP \
 	    -o ${PRJ}/raw_snps.vcf
@@ -233,21 +319,21 @@ if $HARDFILTER; then
 	#
 	#
 	# Apply the filter to the SNP call set
-	java $MEM -jar ${GATK} -R ${RefGENOME} -T VariantFiltration \
+	java ${MEM} -jar ${GATK} -R ${RefGENOME} -T VariantFiltration \
 	    -V ${PRJ}/raw_snps.vcf \
 	    --filterExpression "QD < 2.0 || FS > 60.0 || MQ < 40.0 || HaplotypeScore > 13.0 || MappingQualityRankSum < -12.5 || ReadPosRankSum < -8.0" \
 	    --filterName "my_snp_filter" \
 	    -o ${PRJ}/filtered_snps.vcf
 
 	# Extract the Indels from the call set
-	java $MEM -jar ${GATK} -R ${RefGENOME} -T SelectVariants -nt $NCORES \
+	java ${MEM} -jar ${GATK} -R ${RefGENOME} -T SelectVariants -nt $NCORES \
 	-V $PRJ.snps.vcf \
 	-selectType INDEL \
 	-o ${PRJ}raw_indels.vcf
 	#
 	#
 	# Apply the filter to the Indel call set
-	java $MEM -jar ${GATK} -R ${RefGENOME} -T VariantFiltration \
+	java ${MEM} -jar ${GATK} -R ${RefGENOME} -T VariantFiltration \
 	-V ${PRJ}/raw_indels.vcf \
 	--filterExpression "QD < 2.0 || FS > 200.0 || ReadPosRankSum < -20.0" \
 	--filterName "my_indel_filter" \
@@ -256,7 +342,7 @@ fi
 
 if $PHASETRANSM; then
 	#snps trio
-	java $MEM -jar ${GATK} -R ${RefGENOME} -T PhaseByTransmission -ped ${PRJ}/${PRJ}.ped \
+	java ${MEM} -jar ${GATK} -R ${RefGENOME} -T PhaseByTransmission -ped ${PRJ}/${PRJ}.ped \
 	-V ${PRJ}/filtered_snps.vcf \
 	-o ${PRJ}/trio.snps.phase.vcf
 
@@ -268,7 +354,7 @@ if $PHASETRANSM; then
   ${PSEQ}/pseq ${PRJ}//trio.snps.pass.vcf v-stats > ${PRJ}/snps.qc.stats
   
 	#indels trio
-	java $MEM -jar ${GATK} -R ${RefGENOME} -T PhaseByTransmission -ped ${PRJ}/${PRJ}.ped \
+	java ${MEM} -jar ${GATK} -R ${RefGENOME} -T PhaseByTransmission -ped ${PRJ}/${PRJ}.ped \
 	-V ${PRJ}/filtered_indels.vcf \
 	-o ${PRJ}/trio.indels.phase.vcf
 
@@ -283,13 +369,13 @@ fi
 cd ~/bin/snpEff/
 if $RISKASSESS; then
 	# from cureFFI pipeline
-	#java $MEM -jar snpEff.jar eff -v -i vcf -o vcf hg19 ${WORK}/trio.snps.phase.vcf > ${WORK}/trio.snpeff.vcf
+	#java ${MEM} -jar snpEff.jar eff -v -i vcf -o vcf hg19 ${WORK}/trio.snps.phase.vcf > ${WORK}/trio.snpeff.vcf
 	# otherwise...
 	# we annotate:
 	# loss of function and nonsense mediated decay predictions (by adding the "-lof" command line option);
 	# protein domain annotations from the curated NextProt database (option "-nextProt"); as well as
 	# putative transcription factor binding sites from the ENSEMBL 'Regulatory Build' and Jaspar database (option "-motif"). 
-	java $MEM -jar snpEff.jar \
+	java ${MEM} -jar snpEff.jar \
 	  -v \
 	  -lof \
 	  -motif \
@@ -298,7 +384,7 @@ if $RISKASSESS; then
 	  GRCh37.74 \
 	  ${WORK}/${PRJ}/trio.${TYPE}.pass.vcf > ${WORK}/${PRJ}/${TYPE}/trio.snpeff_step1.vcf
 	
-	java $MEM -jar SnpSift.jar \
+	java ${MEM} -jar SnpSift.jar \
 	  caseControl \
 	  -v \
 	  -tfam ${WORK}/${PRJ}/${PRJ}.ped \
